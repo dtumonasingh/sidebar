067450fd92202651eec1631a0b9289ab2c5aa1fc
dtumonasingh <dtumonasingh@gmail.com>
2022-05-25 16:38:08 +0530

    Add titles and logos with transition

--- app.js
+++ app.js
@@ -1,8 +1,11 @@
 const sidebar = document.getElementById('sidebar');
 const closeButton = document.getElementById('close-button');
 const sidebarItemsText = document.getElementsByClassName('sidebar__items--text')
+const titles = document.getElementsByClassName('title')
+const circle = document.getElementsByClassName('circles')
 const messageSection = document.getElementById('message-section');
 const sideBarItemsSection = document.getElementById('sidebar-items-section')
+const companyInfo = document.getElementById('company-info')
 
 
 sidebar.addEventListener('click', function(){
@@ -14,6 +17,13 @@ sidebar.addEventListener('click', function(){
       });
     messageSection.classList.add('message-section--wide')
     sideBarItemsSection.classList.add('sidebar__items-section--wide')
+    Array.prototype.forEach.call(titles, a => {
+      a.classList.add('flex-start')
+    });
+    Array.prototype.forEach.call(circle, a => {
+      a.classList.add('flex-start')
+    });
+    companyInfo.classList.add('flex-start')
 })
 
 closeButton.addEventListener('click', function(event) {
@@ -25,5 +35,13 @@ closeButton.addEventListener('click', function(event) {
       });
     messageSection.classList.remove('message-section--wide')
     sideBarItemsSection.classList.remove('sidebar__items-section--wide')
+    Array.prototype.forEach.call(titles, a => {
+      a.classList.remove('flex-start')
+    });
+    Array.prototype.forEach.call(circle, a => {
+      a.classList.add('flex-start')
+    });
+
+    companyInfo.classList.remove('flex-start')
 
 })
